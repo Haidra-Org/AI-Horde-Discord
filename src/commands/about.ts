3d066dee153d3277258c80c80acad2b0c829da43
Divided by Zer0 <mail@dbzer0.com>
2023-01-26 00:01:23 +0100

    trusted

--- src/commands/about.ts
+++ src/commands/about.ts
@@ -19,8 +19,8 @@ export default class extends Command {
     override async run(ctx: CommandContext): Promise<any> {
         const embed = new EmbedBuilder({
             color: Colors.Blue,
-            title: "Unofficial Stable Horde Discord Bot",
-            description: `This Discord Bot was made by Zelda_Fan#0225 with <3\nYou can [view the code on GitHub](https://github.com/ZeldaFan0225/Stable_Horde_Discord) **but there is not guarantee that this instance is unmodified**.\nIf you find any bugs you can [report them on GitHub](https://github.com/ZeldaFan0225/Stable_Horde_Discord/issues).\n\n**Bot Version** \`${ctx.client.bot_version}\`\n**Package Version** \`${ctx.stable_horde_manager.VERSION}\`\n\nThis bot currently is in ${ctx.client.guilds.cache.size} servers`
+            title: "Stable Horde Discord Bot",
+            description: `This Discord Bot was made by Zelda_Fan#0225 with <3. It is hosted by db0#1625\nYou can [view the code on GitHub](https://github.com/ZeldaFan0225/Stable_Horde_Discord).\nIf you find any bugs you can [report them on GitHub](https://github.com/ZeldaFan0225/Stable_Horde_Discord/issues).\n\n**Bot Version** \`${ctx.client.bot_version}\`\n**Package Version** \`${ctx.stable_horde_manager.VERSION}\`\n\nThis bot currently is in ${ctx.client.guilds.cache.size} servers`
         })
         return ctx.interaction.reply({
             embeds: [embed],
